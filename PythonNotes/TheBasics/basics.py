a658a4605295fca8f9c7b392bc6fcb5eb3a14e47
minefarmer <pgoldfarm@yahoo.com>
2020-09-02 20:08:42 -0500

    dishes to do

---
+++ PythonNotes/TheBasics/basics.py
@@ -0,0 +1,8 @@
+monday_temperatures = (9, 8, 7)
+student_grades ={"Mary": 9.1, "Sim": 8.8, "John": 7.5}
+monday_temperatures2.append(6)
+
+mysum =sum(student_grades.values())
+length = len(student_grades)
+mean = mysum / length
+print(monday_temperatures)
