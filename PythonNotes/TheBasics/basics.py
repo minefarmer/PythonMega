78141dce5f1d5e4added5ef3448145a9d2ddd221
minefarmer <pgoldfarm@yahoo.com>
2020-09-03 22:06:17 -0500

    ZZZZZZZZZZZZZZZZZZZ

--- PythonNotes/TheBasics/basics.py
+++ PythonNotes/TheBasics/basics.py
@@ -1,7 +1,15 @@
-monday_temperatures = [1,4,5]
-student_grades = {"Mary": 9.1, "Sim": 8.8, "John": 7.5}
+# monday_temperatures = [1,4,5]
+# student_grades = {"Mary": 9.1, "Sim": 8.8, "John": 7.5}
 
-mysum = sum(student_grades.values())
-length = len(student_grades)
-mean = mysum / length
-print(mean)
+# mysum = sum(student_grades.values())
+# length = len(student_grades)
+# mean = mysum / length
+# print(mean)
+
+
+
+def mean(mylist):
+    the_mean = sum(mylist) / len(mylist)
+    return the_mean
+
+print(mean([1, 4, 5]))  # call the function
